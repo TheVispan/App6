5f0140ef1f51f5f706ee298a45aa7e9a93faef68
BPsan <duble7637@gmail.com>
2022-09-08 15:20:09 +0300

    first

--- pupa/pupa/urls.py
+++ pupa/pupa/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.BlogListView.as_view()),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name = 'post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name = 'post_new'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name = 'post_edit'),
+path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name = 'post_edit')
+
 ]
